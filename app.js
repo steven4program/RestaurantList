8b8b39b12ea61d0cca202613a6739c05b0e603d8
steven4program <stevenchang421@gmail.com>
2021-07-04 16:40:32 +0800

    Initialize Project

---
+++ app.js
@@ -0,0 +1,21 @@
+// require packages used in the project
+const express = require("express");
+const app = express();
+const port = 3000;
+
+// require express-handlebars
+const exphbs = require("express-handlebars");
+
+// set template engine
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// routes setting
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+// start and listen on the Express server
+app.listen(port, () => {
+  console.log(`Express is listen on localhost:${port}`);
+});
