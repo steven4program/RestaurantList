76149b2342a31504a194b37d4bd8d42e8b53e1cf
steven4program <stevenchang421@gmail.com>
2021-07-04 19:07:27 +0800

    Render page from external document

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 // require packages used in the project
 const express = require("express");
 const app = express();
-const port = 3000;
-
-// require express-handlebars
 const exphbs = require("express-handlebars");
+const restaurantList = require("./restaurant.json");
+const port = 3000;
 
 // set template engine
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
@@ -15,7 +14,7 @@ app.use(express.static("public"));
 
 // routes setting
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("index", { restaurant: restaurantList.results });
 });
 
 // start and listen on the Express server
